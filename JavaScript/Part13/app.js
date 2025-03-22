4f99cb3e609c5788b22742fbe322bbb5fb7ae16a
Yash Parmar <yashparmar2772004@gmail.com>
2025-03-22 10:53:21 +0530

    some changes

--- JavaScript/Part13/app.js
+++ JavaScript/Part13/app.js
@@ -32,7 +32,7 @@
 // btn.addEventListener("click", async () => {
 //   let image = await getImage();
 //   let img = document.querySelector("#image");
-//   //   img.src = image;
+//   img.src = image;
 //   //*OR
 //   img.setAttribute("src", image);
 // });
@@ -52,15 +52,76 @@
 
 //! Sending Headers :
 
-const urls = "https://icanhazdadjoke.com/";
+// const urls = "https://icanhazdadjoke.com/";
 
-async function getJokes() {
+// async function getJokes() {
+//   try {
+//     const config = { headers: { Accept: "application/json" } };
+//     let res = await axios.get(urls, config);
+//     console.log(res.data);
+//     console.log(res.data.joke);
+//   } catch (e) {
+//     console.log("ERROR :", e);
+//   }
+// }
+
+// Practice (revision):
+
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", async () => {
+//   let fact = await getApi();
+//   console.log(fact);
+
+//   let p = document.querySelector("#result");
+//   p.textContent = fact;
+// });
+
+// let url = "https://catfact.ninja/fact";
+
+// async function getApi() {
+//   try {
+//     let res = await axios.get(url);
+//     return res.data.fact;
+//     // console.log(res.data.fact);
+//   } catch (e) {
+//     console.log("ERROR", e);
+//     return "Fact Error";
+//   }
+// }
+
+//Practice 2 :
+
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", async () => {
+//   let imgUrl = await getImg();
+
+//   let Img = document.querySelector("#image");
+//   Img.src = imgUrl;
+// });
+
+// let url2 = "https://dog.ceo/api/breeds/image/random";
+
+// async function getImg() {
+//   try {
+//     let res = await axios.get(url2);
+//     return res.data.message;
+//   } catch (e) {
+//     console.log("ERROR", e);
+//     return "IMG not found";
+//   }
+// }
+
+//Practice 3 :
+
+let url = "https://world.openfoodfacts.org/api/v0/product/737628064502.json";
+
+async function getFood() {
   try {
-    const config = { headers: { Accept: "application/json" } };
-    let res = await axios.get(urls, config);
-    console.log(res.data);
-    console.log(res.data.joke);
+    let res = await axios.get(url);
+    console.log(res.data.product.image_url);
   } catch (e) {
-    console.log("ERROR :", e);
+    console.log("Error", e);
   }
 }
