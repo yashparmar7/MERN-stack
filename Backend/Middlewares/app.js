079c80eae423c3d6cd3232c2d9acc4558a2c0650
yashparmar7 <yashparmar2772004@gmail.com>
2025-04-19 11:38:26 +0530

    Added all files

--- Backend/Middlewares/app.js
+++ Backend/Middlewares/app.js
@@ -1,10 +1,43 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+//! middleware
+
+//* logger - morgan
+// app.use((req, res, next) => {
+//   // res.send("Middleware done!");
+//   console.log(req.method, req.path, req.hostname);
+//   next();
+// });
+
+//todo  Example :
+const checkToken =
+  ("/api",
+  (req, res, next) => {
+    let { token } = req.query;
+    if (token === "giveaccess") {
+      next();
+    } else {
+      res.status(401).send("Access denied");
+    }
+  });
+
+app.get("/api", checkToken, (req, res) => {
+  res.send("Hello from API");
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello, World! /");
+});
+
+app.get("/random", (req, res) => {
+  res.send("random path");
+});
+
+//! Error middleware
+app.use((req, res) => {
+  res.status(404).send("Page Not found");
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
