c16854e46f1a03898e026d473200b90191074871
yashparmar7 <yashparmar2772004@gmail.com>
2025-06-19 21:40:45 +0530

    Added all files

---
+++ Frontend/ReactJS/basic-app/src/Product.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Product.css";
+
+const Product = () => {
+  return (
+    <div className="product">
+      <h1>Hello I am Title</h1>
+      <p>Hello I am Description</p>
+    </div>
+  );
+};
+
+export default Product;
---
+++ Frontend/ReactJS/basic-app/src/ProductTab.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Product from "./Product";
+
+const ProductTab = () => {
+  return (
+    <>
+      <Product />
+      <Product />
+      <Product />
+    </>
+  );
+};
+
+export default ProductTab;
